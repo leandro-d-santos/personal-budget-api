bb05a514e46cf7623fbc097ed22db511549714f3
leandro-d-santos <leandrosebold16@gmail.com>
2023-10-03 21:23:08 -0300

    Startup project

---
+++ Api/Program.cs
@@ -0,0 +1,20 @@
+namespace Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
